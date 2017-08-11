045fa1689890f755daf165a15b8eb9a0833d3114
冯冬冬 <fengdd@hdsxtech.com>
2017-08-11 10:27:31 +0800

    # 处理工具参数和销毁事件

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
  * Created by FDD on 2017/7/28.
  * @desc 图层滤镜
  */
+
+import * as Events from 'nature-dom-util/src/Events'
+
 ol.interaction.LayerSpyglass = function (params) {
   this.options = params || {}
   if (this.options['spyLayer']) {
@@ -14,8 +17,43 @@ ol.interaction.LayerSpyglass = function (params) {
    * 默认滤镜半径
    * @type {number}
    */
-  this.radius = (this.options['radius'] && typeof this.options['radius'] === 'number') ? this.options['radius'] : 75
+  this.radius = (typeof this.options['radius'] === 'number') ? this.options['radius'] : 75
 
+  /**
+   * 滤镜最大半径
+   * @type {number}
+   */
+  this.minRadius = (typeof this.options['minRadius'] === 'number') ? this.options['minRadius'] : 150
+
+  /**
+   * 滤镜最小半径
+   * @type {number}
+   */
+  this.maxRadius = (typeof this.options['maxRadius'] === 'number') ? this.options['maxRadius'] : 25
+
+  /**
+   * 滤镜边框宽度
+   * @type {number}
+   */
+  this.lineWidth = (typeof this.options['lineWidth'] === 'number') ? this.options['lineWidth'] : 5
+
+  /**
+   * 滤镜边线颜色
+   * @type {number}
+   */
+  this.strokeStyle = (this.options['strokeStyle'] ? this.options['strokeStyle'] : 'rgba(0, 0, 0, 0.5)')
+
+  /**
+   * 滤镜放大对应键值
+   * @type {number}
+   */
+  this.zoomInKeyCode = this.options['zoomInKeyCode'] !== undefined ? this.options['zoomInKeyCode'] : 38
+
+  /**
+   * 滤镜缩小对应键值
+   * @type {number}
+   */
+  this.zoomOutKeyCode = this.options['zoomOutKeyCode'] !== undefined ? this.options['zoomOutKeyCode'] : 40
   /**
    * 当前鼠标位置
    * @type {null}
@@ -51,33 +89,72 @@ ol.interaction.LayerSpyglass.handleEvent_ = function (evt) {
  * @private
  */
 ol.interaction.LayerSpyglass.prototype.initEvents_ = function () {
-  this.getMap().getTargetElement().addEventListener('mouseout', () => {
-    this.mousePosition = null
-    this.getMap().render()
-  })
+  Events.listen(this.getMap().getTargetElement(), 'mouseout', this.handleMouseOut_, this)
+  Events.listen(document, 'keydown', this.handleKeyDown_, this)
   // before rendering the layer, do some clipping
-  this.spyLayer.on('precompose', event => {
-    let ctx = event.context
-    let pixelRatio = event.frameState.pixelRatio
-    ctx.save()
-    ctx.beginPath()
-    if (this.mousePosition) {
-      // only show a circle around the mouse
-      ctx.arc(this.mousePosition[0] * pixelRatio, this.mousePosition[1] * pixelRatio,
-        this.radius * pixelRatio, 0, 2 * Math.PI)
-      ctx.lineWidth = 5 * pixelRatio
-      ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)'
-      ctx.stroke()
-    }
-    ctx.clip()
-  })
-
+  this.spyLayer.on('precompose', this.handlePrecompose_, this)
   // after rendering the layer, restore the canvas context
-  this.spyLayer.on('postcompose', event => {
-    let ctx = event.context
-    ctx.restore()
-  })
+  this.spyLayer.on('postcompose', this.handlePostcompose_, this)
 }
+
+/**
+ * 处理鼠标移除事件
+ * @param event
+ * @private
+ */
+ol.interaction.LayerSpyglass.prototype.handleMouseOut_ = function (event) {
+  this.mousePosition = null
+  this.getMap().render()
+}
+
+/**
+ * 处理键盘事件
+ * @param event
+ * @private
+ */
+ol.interaction.LayerSpyglass.prototype.handleKeyDown_ = function (event) {
+  if (event.which === this.zoomInKeyCode) {
+    this.radius = Math.min(this.radius + 5, 150)
+    this.getMap().render()
+    event.preventDefault()
+  } else if (event.which === this.zoomOutKeyCode) {
+    this.radius = Math.max(this.radius - 5, 25)
+    this.getMap().render()
+    event.preventDefault()
+  }
+}
+
+/**
+ * 图层开始渲染之前事件处理
+ * @param event
+ * @private
+ */
+ol.interaction.LayerSpyglass.prototype.handlePrecompose_ = function (event) {
+  let ctx = event.context
+  let pixelRatio = event.frameState.pixelRatio
+  ctx.save()
+  ctx.beginPath()
+  if (this.mousePosition) {
+    // only show a circle around the mouse
+    ctx.arc(this.mousePosition[0] * pixelRatio, this.mousePosition[1] * pixelRatio,
+      this.radius * pixelRatio, 0, 2 * Math.PI)
+    ctx.lineWidth = this.lineWidth * pixelRatio
+    ctx.strokeStyle = this.strokeStyle
+    ctx.stroke()
+  }
+  ctx.clip()
+}
+
+/**
+ * 开始渲染时的事件处理
+ * @param event
+ * @private
+ */
+ol.interaction.LayerSpyglass.prototype.handlePostcompose_ = function (event) {
+  let ctx = event.context
+  ctx.restore()
+}
+
 /**
  * 设置地图对象
  * @param map
@@ -87,6 +164,12 @@ ol.interaction.LayerSpyglass.prototype.setMap = function (map) {
     ol.interaction.Interaction.prototype.setMap.call(this, map)
     this.initEvents_()
   } else {
-    throw Error('传入的不是地图对象！')
+    Events.unListen(this.getMap().getTargetElement(), 'mouseout', this.handleMouseOut_, this)
+    Events.unListen(document, 'keydown', this.handleKeyDown_, this)
+    // before rendering the layer, do some clipping
+    this.spyLayer.un('precompose', this.handlePrecompose_, this)
+    // after rendering the layer, restore the canvas context
+    this.spyLayer.un('postcompose', this.handlePostcompose_, this)
+    ol.interaction.Interaction.prototype.setMap.call(this, map)
   }
 }
